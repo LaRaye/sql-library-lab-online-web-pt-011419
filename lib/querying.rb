30f3109c5997c4a61667840c4136146a845ec875
LaRaye Johnson <lnj2105@gmail.com>
2019-03-29 17:27:26 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -37,13 +37,6 @@ def select_name_and_series_subgenres_of_authors
   ON series.subgenre_id = subgenres.id;"
 end
 
-# def select_name_and_series_subgenres_of_authors
-#   "SELECT authors.name, subgenres.name FROM authors
-#   INNER JOIN subgenres
-#   ON authors.subgenre_id = subgenres.id
-#   GROUP BY authors.name;"
-# end
-
 def select_series_title_with_most_human_characters
   "SELECT series.title FROM characters
   INNER JOIN series
